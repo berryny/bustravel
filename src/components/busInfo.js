22ac634c3f4b932bfbd533949aa5f05d28f410f7
J Desire <judi@redesigningtech.com>
2019-10-16 17:19:13 -0400

    departures and arrival

--- src/components/busInfo.js
+++ src/components/busInfo.js
@@ -1,16 +1,33 @@
 import React, { Component } from 'react';
 import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+
+import '../App.css';
+
+const getBrowserLang = navigator.language || navigator.userLanguage;
+
 class BusInfo extends Component {
   constructor(props) {
     super(props)
+    this.state = {
+      feed: {},
+      city: [],
+      departures: [],
+      location: [],
+      operator: []
+    }
   }
+
 
   componentDidMount(){
     console.log('this.props.data',this.props.data);
-    // const url = "https://napi.busbud.com/x-departures/"+this.state.obj.orgin+"/"+this.state.obj.destination+"/"+this.state.obj.date+"?adult=1&child=0&senior=0&lang="+getBrowserLang+"&currency="+this.state.feed.getUsersIPFeed.currency;
-    const url_NY2CA = "https://napi.busbud.com/x-departures/dr5reg/f25dvk/2020-08-02?adult=1&child=0&senior=0&lang=en&currency=USD";
-    console.log('url',url_NY2CA);
-    fetch(url_NY2CA, {
+    const getObj = this.props.data
+    // console.log('getObj',getObj.origin);
+    const url = "https://napi.busbud.com/x-departures/"+getObj.origin+"/"+getObj.destination+"/"+getObj.date+"?adult=1&child=0&senior=0&lang="+getBrowserLang+"&currency=USD";
+    // const url = "https://napi.busbud.com/x-departures/dr5reg/f25dvk/2020-08-02?adult=1&child=0&senior=0&lang=en&currency=USD";
+    console.log('url',url);
+    fetch(url, {
       method: "GET",
       headers: {
         Accept: 'application/vnd.busbud+json; version=2; profile=https://schema.busbud.com/v2/',
@@ -21,8 +38,15 @@ class BusInfo extends Component {
       return response.json()
     }, function(error) {
       console.log('error', error.message );
-    }).then(function(data){
-      console.log('data',data, data.departures);
+    }).then(data => {
+      console.log('info',data);
+      this.setState({
+        feed: data,
+        city: data.cities,
+        departures: data.departures,
+        location: data.locations,
+        operator: data.operators
+      });
       // const departures = data.departures;
       // const operators = data.operators;
 
@@ -51,10 +75,52 @@ class BusInfo extends Component {
     })
 
   }
-
+  // <p>departure time, the arrival time, the location name</p>
   render() {
+
+    const display_city = (cities_id) =>
+      this.state.city.map((cityObj, n) => {
+        if ( cities_id === cityObj.id) {
+          return cityObj.name;
+        }
+      })
+
+    const display_location = (location_id) =>
+      this.state.location.map((local, l) => {
+        if (location_id === local.id) {
+          return local.address[0];
+        }
+      })
+
+    const display_buses = this.state.departures.map((ele, idx) =>
+    {
+      {/*Show the departure time, the arrival time, the location name and the price (use prices.total of the departure)*/}
+      console.log('ele, idx',ele, idx, this.state.feed.origin_city_id);
+        return (
+          <Row className="justify-content-md-center departures p-2 mb-3" key={idx}>
+            <Col xs={12} md={8}>
+              <div className="d-flex flex-row mb-3 justify-content-start">
+                <div className="departuretime mr-3">{ele.departure_time}</div>
+                <div>
+                   <span>{display_city(this.state.feed.origin_city_id)}</span> - <span>{display_location(ele.origin_location_id)}</span>
+                 </div>
+              </div>
+              <div className="d-flex flex-row mb-3 justify-content-start">
+                <div className="departuretime mr-3">{ele.arrival_time}</div>
+                <div>
+                   <span>{display_city(this.state.feed.destination_city_id)}</span> - <span>{display_location(ele.destination_location_id)}</span>
+                 </div>
+              </div>
+            </Col>
+            <Col xs={6} md={4}><p>the price (use prices.total of the departure)</p></Col>
+          </Row>
+        )
+    }
+  )
     return (
-      <div><h1>hi</h1></div>
+      <div>
+        <Container><div id="departures" className="m-4">{display_buses}</div></Container>
+      </div>
 
     );
   }
--- src/components/home.js
+++ src/components/home.js
@@ -11,8 +11,6 @@ import Data from '../json/bus_travel_schedules.json';
 import CodeData from '../json/country_code.json';
 import BusInfo from '../components/busInfo'
 
-const getBrowserLang = navigator.language || navigator.userLanguage;
-
 class Home extends Component {
 
   constructor(props){
@@ -24,7 +22,6 @@ class Home extends Component {
       getFormValues: null,
       value: ''
     }
-    //this.renderSchedule = this.renderSchedule.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
@@ -49,24 +46,15 @@ class Home extends Component {
     const form = event.currentTarget;
     let forms = document.getElementsByClassName('needs-validation');
 
-    // this.state = {
-    //   orgin: data.get('bustkts.SelectOrigin'),
-    //   destination: data.get('bustkts.SelectDestination'),
-    //   date: data.get('bustkts.SelectDate'),
-    //   passenger: data.get('bustkts.SelectPassenger')
-    // }
-    // console.log('getFormValues',this.state.getFormValues);
-    // this.another()
     let getOrigin = data.get('bustkts.SelectOrigin'),
       getDestination = data.get('bustkts.SelectDestination')
 
     if (form.checkValidity() === true) {
       if (getOrigin === getDestination){
-        console.log('make a selection', event.target);
+        console.log('select different cities', event.target);
         event.target.reset()
       } else {
-        console.log('happy')
-
+        console.log('happy travels')
           this.setState({
             getFormValues: {
               origin: data.get('bustkts.SelectOrigin'),
@@ -75,55 +63,8 @@ class Home extends Component {
               passenger: data.get('bustkts.SelectPassenger')
             }
           });
-
       }
     }
-  }
-
-  getSchedule(){
-    console.log('obj',this.state);
-    // this.state = {obj, feed}
-    // const url = "https://napi.busbud.com/x-departures/"+this.state.obj.orgin+"/"+this.state.obj.destination+"/"+this.state.obj.date+"?adult=1&child=0&senior=0&lang="+getBrowserLang+"&currency="+this.state.feed.getUsersIPFeed.currency;
-    // console.log('url',url);
-    // fetch(url, {
-    //   method: "GET",
-    //   headers: {
-    //     Accept: 'application/vnd.busbud+json; version=2; profile=https://schema.busbud.com/v2/',
-    //     'X-Busbud-Token': 'PARTNER_AHm3M6clSAOoyJg4KyCg7w'
-    //   }
-    //
-    // }).then(function(response) {
-    //   return response.json()
-    // }, function(error) {
-    //   console.log('error', error.message );
-    // }).then(function(data){
-    //   console.log('data',data, data.departures);
-    //   const departures = data.departures;
-    //   const operators = data.operators;
-    //
-    //   // return data.departures.map((item, o) => (
-    //   //
-    //   //   <Card style={{ width: '18rem' }}>
-    //   //     <Card.Body>
-    //   //       operators.map((op, i) =>
-    //   //       if (item.operator_id === op.id) {
-    //   //         return <Card.Title key={i}>{op.name}</Card.Title>
-    //   //       }
-    //   //
-    //   //     )
-    //   //       <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
-    //   //       <Card.Text>
-    //   //         Some quick example text to build on the card title and make up the bulk of
-    //   //         the card's content.
-    //   //       </Card.Text>
-    //   //       <Card.Link href="#">Card Link</Card.Link>
-    //   //       <Card.Link href="#">Another Link</Card.Link>
-    //   //     </Card.Body>
-    //   //   </Card>
-    //   //
-    //   // ))
-    //
-    // })
   }
 
   render() {
@@ -132,13 +73,13 @@ class Home extends Component {
       <Container>
         <Form className="form-inline needs-validation" onSubmit={this.handleSubmit} novalidate>
           <Form.Group controlId="bustkts.SelectOrigin" className="mr-2">
-            <Form.Label>Departures</Form.Label>
+            <Form.Label>Leaving from </Form.Label>
             <Form.Control as="select" name="bustkts.SelectOrigin" placeholder="Select" required>
               {this.renderOptions(CodeData)}
             </Form.Control>
           </Form.Group>
           <Form.Group controlId="bustkts.SelectDestination" className="mr-2">
-            <Form.Label>Destination</Form.Label>
+            <Form.Label>Going to </Form.Label>
             <Form.Control as="select" name="bustkts.SelectDestination" placeholder="Select" required>
               {this.renderOptions(CodeData)}
             </Form.Control>
